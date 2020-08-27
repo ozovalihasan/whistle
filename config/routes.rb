4147ee02b9857b7b0da1a4dcc64cae4785231414
ozovalihasan <ozovalihasan@gmail.com>
2020-08-27 18:49:19 +0300

    Update routes for whiistle contoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :whiistles
+  resources :whiistles, only: %i[index create]
   resources :users, only: [:show]
   resources :followings, only: %i[create destroy]
   root 'whiistles#index'
