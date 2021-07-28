bad676b2833e823398e56f7b01593842add99e83
Hasan Ozovali <ozovalihasan@gmail.com>
2021-07-28 17:38:08 +0300

    Remove unnecessary assigned variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,8 +273,6 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  github_client_id = Rails.application.credentials.github[:client_id]
-  github_client_secret = Rails.application.credentials.github[:client_secret]
   config.omniauth :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], scope: 'user,public_repo'
 
   # ==> Warden configuration
