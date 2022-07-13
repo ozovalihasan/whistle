5b7ee2816fefe28a60a4d695f9f70657b1a6407e
ozovalihasan <ozovalihasan@gmail.com>
2022-07-14 00:28:56 +0300

    Update the version of the webpacker gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
