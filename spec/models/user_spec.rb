905808b61baf100d30c0981ff93179fea5ff841a
ozovalihasan <ozovalihasan@gmail.com>
2022-12-01 19:25:46 +0300

    Update the test of User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,25 +2,29 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
+    subject { FactoryBot.build(:mock_user) }
+    it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
-    it { should validate_uniqueness_of(:username) }
+    it { should validate_uniqueness_of(:username)  }
     it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:fullname) }
   end
 
   describe 'associations' do
     it { should have_many(:whiistles).dependent(:destroy) }
 
     it { should have_many(:following_relations).dependent(:destroy) }
-    it { should have_many(:following_relations).class_name('Following').with_foreign_key('user_id') }
-    it { should have_many(:followings).through(:following_relations).source(:follower) }
+    it { should have_many(:following_relations).class_name('Relation').with_foreign_key('following_id') }
+    it { should have_many(:followeds).through(:following_relations) }
     it { should have_many(:followed_relations).dependent(:destroy) }
-    it { should have_many(:followed_relations).class_name('Following').with_foreign_key('followed_id') }
-    it { should have_many(:followers).through(:followed_relations).source(:following) }
+    it { should have_many(:followed_relations).class_name('Relation').with_foreign_key('followed_id') }
+    it { should have_many(:followings).through(:followed_relations) }
   end
 
   it 'is valid with valid attributes' do
-    user = User.new(username: 'hasan', fullname: 'Hasan Ozovali')
-    user.update(email: 'hasan@email.com', password: 'aaaaaa', password_confirmation: 'aaaaaa')
+    FactoryBot.reload
+    FactoryBot.create(:mock_user)
+    
     expect(User.first).to be_valid
   end
 
