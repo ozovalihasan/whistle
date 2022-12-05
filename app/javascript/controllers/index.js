6ad44d45c3d5be5af814134a233282d3be292c6c
ozovalihasan <ozovalihasan@gmail.com>
2022-12-05 12:18:02 +0300

    Add a stimulus controller to use customized tabs

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,11 @@ import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import Tabs from "./custom_tabs_controller"
 
+eagerLoadControllersFrom("controllers", application)
+application.register('tabs', Tabs)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
