8016655574113aefed7826ed6a334a07aa07d951
ozovalihasan <ozovalihasan@gmail.com>
2022-12-17 22:02:00 +0300

    Update User#main_page_whiistles to use "includes" method

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,13 +25,12 @@ class User < ApplicationRecord
   has_many :followings, through: :following_relations, source: :followed
 
   def main_page_whiistles
-    whiistles_shared_by_user = self.shared_whiistles.select("base_whiistles.*, rewhiistles.created_at AS primary_created_at, 'shared_whiistle' as label").to_sql
-    user_whiistles = self.whiistles.select("base_whiistles.*, base_whiistles.created_at as primary_created_at, 'primary_whiistle' AS label").to_sql
+    whiistles_shared_by_user = self.shared_whiistles.select("base_whiistles.*, rewhiistles.created_at AS primary_created_at, 'shared_whiistle' AS label").to_sql
+    user_whiistles = self.whiistles.select("base_whiistles.*, base_whiistles.created_at AS primary_created_at, 'primary_whiistle' AS label").to_sql
     all_whiistles = "(#{whiistles_shared_by_user}) UNION ALL (#{user_whiistles})"
-    all_whiistles_without_replies = "SELECT * FROM (#{all_whiistles}) AS all_whiistles WHERE all_whiistles.type != 1"
-    ordered_whiistles = "#{all_whiistles_without_replies} ORDER BY primary_created_at DESC"
-    
-    BaseWhiistle.find_by_sql(ordered_whiistles)
+    all_whiistles_without_replies = "(#{all_whiistles}) AS all_whiistles WHERE all_whiistles.type != 1"
+
+    BaseWhiistle.from(all_whiistles_without_replies).select("*").order(primary_created_at: :desc).includes(user:  [{ profile_picture_attachment: :blob }])
   end
   
   def followings_and_user_ids
