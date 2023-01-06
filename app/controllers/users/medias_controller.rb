d17673cf760e95062a1e65dc6a6c004f27db3fcb
ozovalihasan <ozovalihasan@gmail.com>
2023-01-06 14:09:56 +0300

    Refactor whiistles of Users#MediasController

--- app/controllers/users/medias_controller.rb
+++ app/controllers/users/medias_controller.rb
@@ -5,7 +5,7 @@ class Users::MediasController < ApplicationController
   def index
     @followings = @user.followings
     @followers = @user.followers.with_attached_profile_picture
-    @all_whiistles = @user.whiistles.joins(:pictures_attachments).distinct.order(created_at: :desc)
+    @all_whiistles = @user.whiistles.joins(:pictures_attachments).distinct.descending_order
     @paginated_whiistles, @last_page, @page = PaginateWhiistles.call(@all_whiistles, params[:page])
     @relation = current_user.following_relations.find_by(followed_id: @user.id)
   end
