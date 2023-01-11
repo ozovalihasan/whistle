3b3c785d69be7175058ef3c1ef14bb3547a9b445
ozovalihasan <ozovalihasan@gmail.com>
2023-01-11 19:18:05 +0300

    Add a validation to Rewhiistle

--- app/models/rewhiistle.rb
+++ app/models/rewhiistle.rb
@@ -1,4 +1,6 @@
 class Rewhiistle < ApplicationRecord
+  validates :user, uniqueness: { scope: :whiistle }
+  
   belongs_to :whiistle, class_name: "BaseWhiistle", counter_cache: true
   belongs_to :user, counter_cache: true
 end
