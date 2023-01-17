6f5225e1e83dd454cd0f764f51963ba21e53b958
ozovalihasan <ozovalihasan@gmail.com>
2023-01-17 12:05:44 +0300

    Add a test suite for the create view of RelationsController

--- spec/views/relations/create.turbo_stream.erb_spec.rb
+++ spec/views/relations/create.turbo_stream.erb_spec.rb
@@ -2,8 +2,30 @@ require 'rails_helper'
 
 RSpec.describe "relations/create", type: :view do
   
+  before(:each) do
+    FactoryBot.create_pair(:mock_user)
+  end
+  
   describe "renders the create turbo stream view of RelationsController" do
-    # TODO: Add tests
+    it "renders turbo_streams correctly if the relation is saved" do
+      relation = FactoryBot.build(:mock_relation)
+      flash[:notice] = "The relation is saved" if relation.save
+      assign(:relation, relation)
+
+      render 
+      
+      expect(rendered).to match_snapshot('create/successful')  
+    end
+
+    it "renders turbo_streams correctly if the relation is not saved" do
+      relation = FactoryBot.build(:mock_relation, followed_id: User.last.id + 1)
+      flash[:alert] = "The relation is not saved" unless relation.save
+      assign(:relation, relation)
+
+      render 
+
+      expect(rendered).to match_snapshot('create/fail')  
+    end
   end
 
 end
