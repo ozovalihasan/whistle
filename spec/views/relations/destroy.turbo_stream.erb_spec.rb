66f93d505274a95a4f23e82ade35b0a829ef2cd7
ozovalihasan <ozovalihasan@gmail.com>
2023-01-17 13:04:39 +0300

    Add a test suite for the destroy view of RelationsController

--- spec/views/relations/destroy.turbo_stream.erb_spec.rb
+++ spec/views/relations/destroy.turbo_stream.erb_spec.rb
@@ -2,8 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "relations/destroy", type: :view do
   
-  describe "renders the destroy turbo stream view of RelationsController" do
-    # TODO: Add tests
+  before(:each) do
+    FactoryBot.create_pair(:mock_user)
+    FactoryBot.create(:mock_relation)
   end
+  
+  describe "renders the destroy turbo stream view of RelationsController" do
+    it "renders turbo_streams correctly if the relation is destroyed" do
+      relation = Relation.first
+      relation.destroy
+      flash[:notice] = "The relation is destroyed"
+      assign(:relation, relation)
+
+      render 
+      
+      expect(rendered).to match(flash[:notice])
+      expect(rendered).to match_snapshot('destroy')  
+    end
+  end
+
 
 end
