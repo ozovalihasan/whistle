2df35efa12397d421ce798383783f058e2c1cd0a
ozovalihasan <ozovalihasan@gmail.com>
2023-01-18 16:15:17 +0300

    Add infinite scroll feature for users having relations

--- app/controllers/users/followers_controller.rb
+++ app/controllers/users/followers_controller.rb
@@ -3,7 +3,8 @@ class Users::FollowersController < Users::TabsController
   def index
     super
 
-    @followers = @user.followers.with_attached_profile_picture
+    followers = @user.followers.with_attached_profile_picture
+    @paginated_users = PaginateRecords.new(followers, params[:page], user_followers_url(@user))
   end
 
 end
--- app/controllers/users/followings_controller.rb
+++ app/controllers/users/followings_controller.rb
@@ -3,7 +3,8 @@ class Users::FollowingsController < Users::TabsController
   def index
     super
 
-    @followings = @user.followings.with_attached_profile_picture
+    followings = @user.followings.with_attached_profile_picture
+    @paginated_users = PaginateRecords.new(followings, params[:page], user_followings_url(@user))
   end
   
 end
