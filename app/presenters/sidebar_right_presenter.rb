33a6ee8ec28c543ad6a83970d982abea1292106b
ozovalihasan <ozovalihasan@gmail.com>
2023-01-30 11:49:29 +0300

    Fix the sidebar presenter

--- app/presenters/sidebar_right_presenter.rb
+++ app/presenters/sidebar_right_presenter.rb
@@ -4,6 +4,6 @@ class SidebarRightPresenter
   def initialize(user, user2)
     @user = user
     @relation = user2.following_relations.find_by(followed_id: user.id)
-    @random_followings = user.followers.limit(5).order("RANDOM()") - [ user2 ]
+    @random_followings = user.followings.limit(5).order("RANDOM()") - [ user2 ]
   end
 end
