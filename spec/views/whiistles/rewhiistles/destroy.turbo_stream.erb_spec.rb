c09c63afacb3f84ec82ba704ff0da54f7f12ad54
ozovalihasan <ozovalihasan@gmail.com>
2023-01-31 19:28:27 +0300

    Update the test of the destroy.turbo_stream view of Whiistles::RewhiistlesController

--- spec/views/whiistles/rewhiistles/destroy.turbo_stream.erb_spec.rb
+++ spec/views/whiistles/rewhiistles/destroy.turbo_stream.erb_spec.rb
@@ -3,7 +3,30 @@ require 'rails_helper'
 RSpec.describe "whiistles/rewhiistles/destroy.turbo_stream", type: :view do
   
   describe "renders the destroy turbo stream view of Whiistles::RewhiistlesController" do
-    # TODO: Add tests
+    it "renders correctly" do
+      FactoryBot.reload
+      user = FactoryBot.create(:mock_user)
+      FactoryBot.create(:mock_user)
+
+      main_whiistle = FactoryBot.create(:mock_whiistle, user: user)
+      rewhiistle = FactoryBot.create(:mock_rewhiistle)
+      assign(:rewhiistle, rewhiistle)
+
+      rewhiistle.destroy!
+
+      flash[:notice] = "The rewhiistle is destroyed"
+      
+      subcomponents = [ Whiistles::CreateRewhiistleOrQuoteButtonComponent ]
+      mock_components(subcomponents)
+      
+      render
+
+      expect(rendered).to match_snapshot('destroy_turbo_stream')
+      expect(rendered).to include("The rewhiistle is destroyed").exactly(1).times
+      expect(rendered).to include("Whiistles::CreateRewhiistleOrQuoteButtonComponent(whiistle: Whiistle)")
+      expect(rendered).to match(/button(.|\n)*Rewhiistle(.|\n)*button/)
+
+    end
   end
 
 end
