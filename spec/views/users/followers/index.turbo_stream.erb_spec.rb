8b472d89fa2c7fb70e034192589427da3869803e
ozovalihasan <ozovalihasan@gmail.com>
2023-02-01 12:25:21 +0300

    Update the test of the index.turbo_stream view of Users::FollowersController

--- spec/views/users/followers/index.turbo_stream.erb_spec.rb
+++ spec/views/users/followers/index.turbo_stream.erb_spec.rb
@@ -3,7 +3,26 @@ require 'rails_helper'
 RSpec.describe "users/followers/index.turbo_stream", type: :view do
   
   describe "renders the index(turbo_stream) view of Users::FollowersController" do
-    # TODO: Add tests
+    it "renders correctly" do
+      FactoryBot.reload
+      user = FactoryBot.create(:mock_user)
+      user2 = FactoryBot.create(:mock_user)
+      user3 = FactoryBot.create(:mock_user)
+      create_list(:mock_relation, 2, followed: user3)
+      
+      followers = user3.followers
+      paginated_users = PaginateUsers.new(followers, 1, user_followers_url(user3))
+      assign(:paginated_users, paginated_users)
+
+      sign_in user      
+
+      render
+
+      expect(rendered).to match_snapshot('index_turbo_stream')
+      expect(rendered).to include('mock_fullname_1').exactly(1).times
+      expect(rendered).to include('mock_fullname_2').exactly(1).times
+
+    end
   end
 
 end
