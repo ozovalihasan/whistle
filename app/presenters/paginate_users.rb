b8a00a25173c93a598912a886759aeb8880da268
ozovalihasan <ozovalihasan@gmail.com>
2023-02-04 21:51:58 +0300

    Refactor PaginateUsers presenter

--- app/presenters/paginate_users.rb
+++ app/presenters/paginate_users.rb
@@ -1,10 +1,10 @@
 class PaginateUsers < PaginateRecords
-  attr_reader :component
+  attr_reader :cur_user
   
   def initialize(original_records, page, url, cur_user)
     super(original_records, page, url)
 
-    @component = Users::UserComponent.with_collection records, cur_user: cur_user
+    @cur_user = cur_user
   end
   
 end
