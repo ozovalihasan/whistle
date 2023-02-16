55fc2cfd2ad24a8adf0b0de99fd0bcfaaffcbd95
ozovalihasan <ozovalihasan@gmail.com>
2023-02-16 18:31:48 +0300

    Update PaginateWhiistles

--- app/presenters/paginate_whiistles.rb
+++ app/presenters/paginate_whiistles.rb
@@ -1,6 +1,6 @@
 class PaginateWhiistles < PaginateRecords
   attr_reader :type
-  attr_accessor :partial_name
+  attr_accessor :component_class
   
   def initialize(original_records, page, url, type = nil)
     super(original_records, page, url)
@@ -11,14 +11,14 @@ class PaginateWhiistles < PaginateRecords
   
   
   def set_for_whiistles_index_page
-    self.partial_name = Whiistles::ListWhiistlesForWhiistlesIndexPageComponent
+    self.component_class = Whiistles::ListWhiistlesForWhiistlesIndexPageComponent
   end
 
   def set_basic
-    self.partial_name = Whiistles::ListWhiistlesComponent
+    self.component_class = Whiistles::ListWhiistlesComponent
   end
 
   def set_connected
-    self.partial_name = Whiistles::ListWhiistlesWithRelatedWhiistlesComponent
+    self.component_class = Whiistles::ListWhiistlesWithRelatedWhiistlesComponent
   end
 end
