9a0e997f95c8f9f218998436dd4ae6af26b10658
ozovalihasan <ozovalihasan@gmail.com>
2023-02-16 22:10:29 +0300

    Remove an unused component

--- app/components/shared/paginate_whiistles_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Shared::PaginateWhiistlesComponent < ApplicationComponent
-  def initialize(paginated_whiistles:)
-    @type = paginated_whiistles.type
-    @user = paginated_whiistles.user
-    @whiistles = paginated_whiistles.records
-  end
-end
