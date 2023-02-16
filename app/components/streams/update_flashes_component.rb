4e8b569a418a5a4ca879aa19f0dd48c89cd56013
ozovalihasan <ozovalihasan@gmail.com>
2023-02-16 18:22:48 +0300

    Add Turbo::StreamsHelper into a component

--- app/components/streams/update_flashes_component.rb
+++ app/components/streams/update_flashes_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Streams::UpdateFlashesComponent < ApplicationComponent
+  include Turbo::StreamsHelper
+  
   def initialize(notice:, alert:)
     @notice = notice
     @alert = alert
