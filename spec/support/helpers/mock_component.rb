dad279ab8fc1f9f78987ce7377f2a6428dea2c8a
ozovalihasan <ozovalihasan@gmail.com>
2023-02-17 13:17:00 +0300

    Update the mock component

--- spec/support/helpers/mock_component.rb
+++ spec/support/helpers/mock_component.rb
@@ -3,19 +3,23 @@ require "rspec/mocks/standalone"
 module Helpers
   module ComponentsTest
     class MockComponent < ApplicationComponent
-      def initialize(component, params)
+      def initialize(component, params, &block)
         @name = component.name
         @params= params
       end
     
       def call
-        @name + "(" + @params.map {|param_key, param_value| param_key.to_s + ": " + param_value.class.name}.join(", ") + ")"
+        @name + "(" + params_to_s + ")" + (content ? content : "")
+      end
+
+      def params_to_s
+        @params.map {|param_key, param_value| param_key.to_s + ": " + param_value.class.name}.join(", ")
       end
     end
     
     def mock_components(components)
       components.each do |mocked_class|
-        allow(mocked_class).to receive(:new) {|params| MockComponent.new( mocked_class, params ) }  
+        allow(mocked_class).to receive(:new) {|params| MockComponent.new( mocked_class, params ) }
         allow(mocked_class).to receive(:with_collection) { MockComponent.new( mocked_class ) }    
       end
     end
