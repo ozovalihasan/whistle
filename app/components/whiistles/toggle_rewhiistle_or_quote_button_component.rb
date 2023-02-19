60c7ce773d28f59217660d5eb1d04dfa594f9338
ozovalihasan <ozovalihasan@gmail.com>
2023-02-19 13:15:48 +0300

    Refactor Whiistles::ToggleRewhiistleOrQuoteButtonComponent

--- app/components/whiistles/toggle_rewhiistle_or_quote_button_component.rb
+++ app/components/whiistles/toggle_rewhiistle_or_quote_button_component.rb
@@ -8,4 +8,12 @@ class Whiistles::ToggleRewhiistleOrQuoteButtonComponent < ApplicationComponent
     @current_user_presenter = current_user_presenter
   end
 
+  def active_button
+    if @current_user_presenter.rewhiistled? @whiistle
+      Whiistles::DestroyRewhiistleOrQuoteButtonComponent.new(whiistle: @whiistle)
+    else
+      Whiistles::CreateRewhiistleOrQuoteButtonComponent.new whiistle: @whiistle
+    end
+  end
+  
 end
