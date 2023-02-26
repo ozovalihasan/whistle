4d1585c6e774d25b607144bc1b27450c889dde68
ozovalihasan <ozovalihasan@gmail.com>
2023-02-26 15:47:15 +0300

    Update instance variables of MockComponents::SimpleFormComponent

--- spec/support/mock_components/simple_form_component.rb
+++ spec/support/mock_components/simple_form_component.rb
@@ -1,12 +1,14 @@
 module MockComponents
   class SimpleFormComponent < ApplicationComponent
-    def initialize(test_component_class:)
-      @test_component_class = test_component_class
+    def initialize(component_class:, form_key:, **kwargs)
+      @component_class = component_class
+      @form_key = form_key
+      @kwargs = kwargs
     end
 
     def call
       simple_form_for Whiistle.new do |form|
-        render @test_component_class.new(form: form) 
+        render @component_class.new(@form_key => form, **@kwargs) 
       end
     end
     
