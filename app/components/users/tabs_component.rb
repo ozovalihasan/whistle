9cb3b39f7f0396df51e8875c33aca31d1e2dd394
ozovalihasan <ozovalihasan@gmail.com>
2023-02-27 15:11:33 +0300

    Add an instance variable 'current_user_presenter' to WhiistlesTabPresenter

--- app/components/users/tabs_component.rb
+++ app/components/users/tabs_component.rb
@@ -4,21 +4,19 @@ class Users::TabsComponent < ApplicationComponent
   include WhiistlesHelper
   include Turbo::FramesHelper
   
-  def initialize(tab_presenter:, sidebar_right_presenter:, current_user_presenter: nil)
+  def initialize(tab_presenter:, sidebar_right_presenter:)
     @tab_presenter = tab_presenter
     @paginated_records = tab_presenter.paginated_records
     @user = tab_presenter.user
     @selected_tab = tab_presenter.name
 
     @sidebar_right_presenter = sidebar_right_presenter
-    @current_user_presenter = current_user_presenter
   end
 
   def tab_body
     if @paginated_records.class == PaginateWhiistles
       Users::WhiistlesTabComponent.new(
-        tab_presenter: @tab_presenter, 
-        current_user_presenter: @current_user_presenter
+        tab_presenter: @tab_presenter
       ) 
     elsif @paginated_records.class == PaginateUsers
       Users::UsersHavingRelationsComponent.new(
--- app/components/users/whiistles_tab_component.rb
+++ app/components/users/whiistles_tab_component.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Users::WhiistlesTabComponent < ApplicationComponent
-  def initialize(tab_presenter:, current_user_presenter:)
+  def initialize(tab_presenter:)
     @tab_presenter = tab_presenter
     @paginated_whiistles = tab_presenter.paginated_records
-    @current_user_presenter = current_user_presenter
+    @current_user_presenter = tab_presenter.current_user_presenter
   end
 
 end
