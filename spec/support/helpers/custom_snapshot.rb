961b04bca270a4ccfdded09d668a3248555c0991
ozovalihasan <ozovalihasan@gmail.com>
2023-03-01 15:33:54 +0300

    Add a default argument for match_custom_snapshot

--- spec/support/helpers/custom_snapshot.rb
+++ spec/support/helpers/custom_snapshot.rb
@@ -1,6 +1,6 @@
 module Helpers
   module Snapshot
-    def match_custom_snapshot(snapshot_name, config = {})
+    def match_custom_snapshot(snapshot_name = "default", config = {})
       component_file_match = RSpec.current_example.metadata[:file_path].match(/([^\/]*)_spec.rb/)[1]
       match_snapshot(component_file_match << "/" << snapshot_name, config)
     end
