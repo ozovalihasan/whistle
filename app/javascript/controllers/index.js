adf07d071c461188a44100e56eec9437a602a058
ozovalihasan <ozovalihasan@gmail.com>
2023-03-08 02:13:56 +0300

    Update flashes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,7 +4,7 @@ import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import { Dropdown } from "tailwindcss-stimulus-components"
+import { Dropdown, Alert } from "tailwindcss-stimulus-components"
 import Flood from "./flood_controller"
 import Tabs from "./custom_tabs_controller"
 import Modal from "./custom_modal_controller"
@@ -14,6 +14,7 @@ eagerLoadControllersFrom("controllers", application)
 application.register('tabs', Tabs)
 application.register('modal', Modal)
 application.register('dropdown', Dropdown)
+application.register('alert', Alert)
 application.register('flood', Flood)
 application.register('pagination', PaginationController)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
