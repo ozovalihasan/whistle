2b165f07d46dac3f38e0486728e7c1569d6f8760
ozovalihasan <ozovalihasan@gmail.com>
2023-03-13 23:37:56 +0300

    Update the test of the index turbo stream view of Users::FollowingsController

--- spec/views/users/followings/index.turbo_stream.erb_spec.rb
+++ spec/views/users/followings/index.turbo_stream.erb_spec.rb
@@ -3,7 +3,20 @@ require 'rails_helper'
 RSpec.describe "users/followings/index.turbo_stream", type: :view do
   
   describe "renders the index(turbo_stream) view of Users::FollowingsController" do
-    # TODO: Add tests
+    it "renders Shared::InfiniteScrollUsers::Component" do
+      FactoryBot.create_pair(:mock_user)
+      followings = User.all
+      paginated_users = PaginateUsers.new(followings, 1, "", nil)
+      
+      assign(:paginated_users, paginated_users)
+
+      render
+
+      expect(rendered).to match_snapshot('index_turbo_stream')
+      expect(rendered).to include('turbo-stream action="append" target="pagination-body"')
+      expect(rendered).to include('Shared::InfiniteScrollUsers::Component(paginated_users: PaginateUsers)')
+
+    end
   end
 
 end
