24787e94bdcbc44fefac1931665b672371236ad3
ozovalihasan <ozovalihasan@gmail.com>
2023-03-14 13:58:36 +0300

    Update the helper used for snapshot tests

--- spec/support/helpers/snapshot.rb
+++ spec/support/helpers/snapshot.rb
@@ -1,8 +1,26 @@
 module Helpers
   module Snapshot
-    def match_custom_snapshot(snapshot_name = "default", config = {})
-      component_file_match = RSpec.current_example.metadata[:file_path].match(/([^\/]*)_spec.rb/)[1]
-      match_snapshot(component_file_match << "/" << snapshot_name, config)
+    def match_custom_snapshot(snapshot_name = nil, config = {})
+
+      if RSpec.current_example.metadata[:type] == :view
+        view_file_match, view_type = RSpec.current_example.metadata[:file_path].match(/([^\/]*)\.(html|turbo_stream)\.erb_spec.rb/)[1..2]
+        if snapshot_name
+          snapshot_name = "/" << snapshot_name 
+        else
+          snapshot_name = "/default"
+        end
+
+        match_snapshot(view_file_match << "/" << view_type << snapshot_name, config)
+      elsif RSpec.current_example.metadata[:type] == :component
+        
+        component_file_match = RSpec.current_example.metadata[:file_path].match(/([^\/]*)_spec.rb/)[1]
+        snapshot_name = "default" if snapshot_name.nil?
+        match_snapshot(component_file_match << "/" << snapshot_name, config)
+
+      else
+        raise StandardError.new "This method is not used for this(#{RSpec.current_example.metadata[:type]}) type"
+      end
+      
     end
   end
 end
