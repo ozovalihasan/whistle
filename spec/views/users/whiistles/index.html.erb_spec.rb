b2a32f44d8e4e4c20a9a38fac77207855fc3c826
ozovalihasan <ozovalihasan@gmail.com>
2023-03-14 16:12:21 +0300

    Update tests of the index view of Users::WhiistlesController

--- spec/views/users/whiistles/index.html.erb_spec.rb
+++ spec/views/users/whiistles/index.html.erb_spec.rb
@@ -3,7 +3,35 @@ require 'rails_helper'
 RSpec.describe "users/whiistles/index", type: :view do
   
   describe "renders the index view of Users::WhiistlesController" do
-    # TODO: Add tests
+  
+    let(:user) {
+      FactoryBot.create(:mock_user)
+    }
+
+    let(:current_user_presenter) { 
+      cur_user = FactoryBot.create(:mock_user)
+      CurrentUserPresenter.new(cur_user)
+    }
+  
+    it "renders correctly" do
+    
+      all_whiistles = Whiistle.all
+      paginated_whiistles = PaginateWhiistles.new(all_whiistles, 1, '')
+
+      whiistle = Whiistle.new
+
+      tab_presenter = WhiistlesTabPresenter.new(user, :mock_name, paginated_whiistles, current_user_presenter, whiistle)
+      assign(:tab_presenter, tab_presenter)
+
+      sidebar_right_presenter = SidebarRightPresenter.new(user, current_user_presenter.current_user)
+      assign(:sidebar_right_presenter, sidebar_right_presenter)
+
+      render
+
+      expect(rendered).to match_custom_snapshot
+      expect(rendered).to include('Users::Tabs::Component(tab_presenter: WhiistlesTabPresenter, sidebar_right_presenter: SidebarRightPresenter)')
+
+    end
   end
 
 end
--- spec/views/users/whiistles/index.turbo_stream.erb_spec.rb
+++ spec/views/users/whiistles/index.turbo_stream.erb_spec.rb
@@ -3,7 +3,28 @@ require 'rails_helper'
 RSpec.describe "users/whiistles/index.turbo_stream", type: :view do
   
   describe "renders the index(turbo_stream) view of Users::WhiistlesController" do
-    # TODO: Add tests
+    let(:current_user_presenter) { 
+      cur_user = FactoryBot.create(:mock_user)
+      CurrentUserPresenter.new(cur_user)
+    }
+  
+    it "renders correctly" do
+    
+      all_whiistles = Whiistle.all
+      
+      paginated_whiistles = PaginateWhiistles.new(all_whiistles, 1, '')
+      paginated_whiistles.set_connected
+      assign(:paginated_whiistles, paginated_whiistles)
+
+      assign(:current_user_presenter, current_user_presenter)
+
+      render
+
+      expect(rendered).to match_custom_snapshot
+      expect(rendered).to include('Whiistles::ListWhiistlesWithRelatedWhiistles::Component(whiistles: ActiveRecord::Relation, current_user_presenter: CurrentUserPresenter)')
+      expect(rendered).to include('turbo-stream action="append" target="pagination-body"')
+
+    end
   end
 
 end
