328938fb9597142f83da23f6152215cc247ff26f
ozovalihasan <ozovalihasan@gmail.com>
2023-03-16 13:04:02 +0300

    Update tests of some views

--- spec/views/whiistles/likes/destroy.turbo_stream.erb_spec.rb
+++ spec/views/whiistles/likes/destroy.turbo_stream.erb_spec.rb
@@ -3,7 +3,22 @@ require 'rails_helper'
 RSpec.describe "whiistles/likes/destroy.turbo_stream", type: :view do
   
   describe "renders the destroy(turbo_stream) view of Users::FollowingsController" do
-    # TODO: Add tests
+    it "renders turbo_streams correctly if the like is saved" do
+      FactoryBot.create(:mock_user)
+      FactoryBot.create(:mock_whiistle)
+      like = FactoryBot.build(:mock_like)
+      like.destroy
+      flash[:notice] = "mock notice"
+      assign(:like, like)
+
+      render 
+
+      expect(rendered).to match_custom_snapshot
+      expect(rendered).to include("StreamAnimations::UpdateAll::Component(targets: String)")
+      expect(rendered).to include("Whiistles::CreateLikeButton::Component(whiistle: Whiistle)")
+      expect(rendered).to include("Streams::UpdateFlashes::Component(notice: String, alert: NilClass)")
+    end
   end
+  
 
 end
--- spec/views/whiistles/pictures/index.html.erb_spec.rb
+++ spec/views/whiistles/pictures/index.html.erb_spec.rb
@@ -3,7 +3,43 @@ require 'rails_helper'
 RSpec.describe "whiistles/pictures/index", type: :view do
   
   describe "renders the index view of Whiistles::PicturesController" do
-    # TODO: Add tests
+    it "renders correctly" do
+      FactoryBot.create(:mock_user)
+      whiistle = FactoryBot.create(:mock_whiistle)
+      whiistle.pictures.attach(
+        [
+          io: File.open(Rails.root.join('app', 'assets', 'images', 'mock-1.jpg')), 
+          filename: 'mock-1.jpg', 
+          content_type: 'image/jpeg'
+        ]
+      )
+      whiistle.pictures.attach(
+        [
+          io: File.open(Rails.root.join('app', 'assets', 'images', 'mock-2.jpg')), 
+          filename: 'mock-2.jpg', 
+          content_type: 'image/jpeg'
+        ]
+      )
+      
+      pictures = whiistle.pictures
+      assign(:pictures, pictures)
+
+      index = 1
+      assign(:index, index)
+
+      render 
+
+      expect(rendered).to match_custom_snapshot
+      expect(rendered).to include('turbo-frame id="modal_body"')
+      expect(rendered).to include('data-controller="tabs"').exactly(1).times
+      expect(rendered).to include('data-tabs-index="1"')
+      expect(rendered).to include('data-action="click->tabs#previous"').exactly(1).times
+      expect(rendered).to include('data-action="click->tabs#next"').exactly(1).times
+      expect(rendered).to include('data-action="mouseover->tabs#change"').exactly(2).times
+      expect(rendered).to include('data-tabs-target="tab"').exactly(2).times
+      expect(rendered).to include('data-tabs-target="panel"').exactly(2).times
+      
+    end
   end
 
 end
