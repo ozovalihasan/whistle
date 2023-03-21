1c840ce8ed30154eaf98340019e80a004b42090d
ozovalihasan <ozovalihasan@gmail.com>
2023-03-21 22:13:45 +0300

    Fix issues raising because of module definitions

--- config/application.rb
+++ config/application.rb
@@ -20,19 +20,21 @@ require 'action_cable/engine'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-class Whistle::Application < Rails::Application
-  # Initialize configuration defaults for originally generated Rails version.
-  config.load_defaults 7.0
+module Whistle
+  class Whistle::Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
 
-  # Configuration for the application, engines, and railties goes here.
-  #
-  # These settings can be overridden in specific environments using the files
-  # in config/environments, which are processed later.
-  #
-  # config.time_zone = "Central Time (US & Canada)"
-  # config.eager_load_paths << Rails.root.join("extras")
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
-  # Don't generate system test files.
-  config.generators.system_tests = nil
-  config.generators.test_framework = 'rspec'
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+    config.generators.test_framework = 'rspec'
+  end
 end
