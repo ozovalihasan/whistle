6c7fb9c574bced1b8a69d1aa77812cebee192315
ozovalihasan <ozovalihasan@gmail.com>
2023-03-21 20:35:21 +0300

    Fix the definition of Users::Tabs::Component

--- app/components/users/tabs/component.rb
+++ app/components/users/tabs/component.rb
@@ -1,33 +1,29 @@
 # frozen_string_literal: true
 
-module Users
-  module Tabs
-    class Component < Application::Component
-      include WhiistlesHelper
-      include Turbo::FramesHelper
+class Users::Tabs::Component < Application::Component
+  include WhiistlesHelper
+  include Turbo::FramesHelper
 
-      def initialize(tab_presenter:, sidebar_right_presenter:)
-        @tab_presenter = tab_presenter
-        @paginate_records = tab_presenter.paginate_records
-        @user = tab_presenter.user
-        @selected_tab = tab_presenter.name
+  def initialize(tab_presenter:, sidebar_right_presenter:)
+    @tab_presenter = tab_presenter
+    @paginate_records = tab_presenter.paginate_records
+    @user = tab_presenter.user
+    @selected_tab = tab_presenter.name
 
-        @sidebar_right_presenter = sidebar_right_presenter
-      end
+    @sidebar_right_presenter = sidebar_right_presenter
+  end
 
-      def tab_body
-        if @paginate_records.instance_of?(PaginateWhiistles)
-          component(
-            'users/whiistles_tab',
-            tab_presenter: @tab_presenter
-          )
-        elsif @paginate_records.instance_of?(PaginateUsers)
-          component(
-            'users/users_having_relations',
-            tab_presenter: @tab_presenter
-          )
-        end
-      end
+  def tab_body
+    if @paginate_records.instance_of?(PaginateWhiistles)
+      component(
+        'users/whiistles_tab',
+        tab_presenter: @tab_presenter
+      )
+    elsif @paginate_records.instance_of?(PaginateUsers)
+      component(
+        'users/users_having_relations',
+        tab_presenter: @tab_presenter
+      )
     end
   end
 end
