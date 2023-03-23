0a264ba3bf0a67fcfb209675af0e119e377d7d50
ozovalihasan <ozovalihasan@gmail.com>
2023-03-23 17:53:46 +0300

    Update components used for flashes

--- spec/components/streams/update_flashes/component_spec.rb
+++ spec/components/streams/update_flashes/component_spec.rb
@@ -7,9 +7,10 @@ RSpec.describe Streams::UpdateFlashes::Component, type: :component do
     render_inline(described_class.new(alert: 'mock_alert', notice: 'mock_notice'))
 
     expect_snapshot_match
-    expect(rendered_content).to match(/Layout::Notice::Component/)
-    expect(rendered_content).to match(/Layout::Alert::Component/)
+    expect(rendered_content).to include("Layout::Flash::Component(message: String)")
+    expect(rendered_content).to include("Layout::Flash::Component(message: String, alert: TrueClass)")
     expect(rendered_content).to match(/turbo-stream/)
     expect(rendered_content).to match(/action="prepend"/)
+    expect(rendered_content).to include('::Component').exactly(2).times
   end
 end
