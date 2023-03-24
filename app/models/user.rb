2d57dec30082b033511e6400d167c93a88ecf90a
ozovalihasan <ozovalihasan@gmail.com>
2023-03-24 12:57:52 +0300

    Update an input used by devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: { case_sensitive: false }, presence: true
   validates :username, uniqueness: true, presence: true
