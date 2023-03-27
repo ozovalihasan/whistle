2a96e15a1f62d91a48fde97ec41e1e96edfcfdd0
ozovalihasan <ozovalihasan@gmail.com>
2023-03-27 23:17:56 +0300

    Update the layout for turbo frame requests of Users::TabsController

--- app/controllers/users/tabs_controller.rb
+++ app/controllers/users/tabs_controller.rb
@@ -4,6 +4,8 @@ class Users::TabsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
 
+  layout :set_layout_for_user_tabs_frame, only: %i[index]
+
   def index
     return if request.format.turbo_stream?
 
@@ -14,5 +16,11 @@ class Users::TabsController < ApplicationController
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def set_layout_for_user_tabs_frame
+    if turbo_frame_request? && (turbo_frame_request_id == "user-tabs")
+      false
+    end
   end
 end
