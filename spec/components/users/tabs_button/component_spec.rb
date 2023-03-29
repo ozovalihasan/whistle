1eead9800bbe9914b0870ac650e4c06a7c82e8a9
ozovalihasan <ozovalihasan@gmail.com>
2023-03-29 12:14:28 +0300

    Update the style of user tabs

--- spec/components/users/tabs_button/component_spec.rb
+++ spec/components/users/tabs_button/component_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Users::TabsButton::Component, type: :component do
       render_inline(described_class.new(url: 'mock_url', text: 'mock_text', active_tab: true))
 
       expect_snapshot_match('active_tab')
-      expect(rendered_content).to match 'block py-2 px-4 text-blue-500 hover:text-blue-700 font-semibold no-underline border-b border-solid border-second'
+      expect(rendered_content).to match 'class="block py-2 w-full h-full font-semibold no-underline border-b border-solid border-second"'
       expect(rendered_content).to match 'mock_text'
     end
   end
@@ -18,7 +18,7 @@ RSpec.describe Users::TabsButton::Component, type: :component do
       render_inline(described_class.new(url: 'mock_url', text: 'mock_text', active_tab: false))
 
       expect_snapshot_match('inactive_tab')
-      expect(rendered_content).to match 'block py-2 px-4 text-blue-500 hover:text-blue-700 font-semibold no-underline'
+      expect(rendered_content).to match 'class="block py-2 w-full h-full font-semibold no-underline "'
       expect(rendered_content).to match 'mock_text'
     end
   end
