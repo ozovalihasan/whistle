e8686b5182f22a8d026318d37806b49d6f1208eb
ozovalihasan <ozovalihasan@gmail.com>
2023-04-01 22:29:33 +0300

    Refactor snapshot helpers

--- spec/support/helpers/snapshot.rb
+++ spec/support/helpers/snapshot.rb
@@ -1,28 +1,23 @@
 # frozen_string_literal: true
 
 module Helpers::Snapshot
-  def expect_snapshot_match(snapshot_name = nil, config = {})
+  def expect_snapshot_match(snapshot_name = "default", config = {})
     case RSpec.current_example.metadata[:type]
     when :view
+      
       view_file_match, view_type = RSpec.current_example.metadata[:file_path].match(%r{([^/]*)\.(html|turbo_stream)\.erb_spec.rb})[1..2]
-      snapshot_name = if snapshot_name
-                        "/#{snapshot_name}" 
-                      else
-                        '/default'
-                      end
 
-      expect(rendered).to match_snapshot(view_file_match << '/' << view_type << snapshot_name, config)
+      expect(rendered).to match_snapshot(view_file_match << '/' << view_type << '/' << snapshot_name, config)
 
     when :component
 
       component_file_match = RSpec.current_example.metadata[:file_path].match(%r{([^/]*)_spec.rb})[1]
-      snapshot_name = 'default' if snapshot_name.nil?
+      
       expect(rendered_content).to match_snapshot(component_file_match << '/' << snapshot_name, config)
 
     when :feature
 
       request_file_match = RSpec.current_example.metadata[:file_path].match(%r{([^/]*)_spec.rb})[1]
-      snapshot_name = 'default' if snapshot_name.nil?
       
       body = find("body").native
       expect(body.attribute('outerHTML') ).to match_snapshot(request_file_match << '/' << snapshot_name, config)
