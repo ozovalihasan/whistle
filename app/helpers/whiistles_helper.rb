66117563d198cd57541e152b231349db6fb83e90
ozovalihasan <ozovalihasan@gmail.com>
2023-04-17 17:05:49 +0300

    Remove check_click helper

--- app/helpers/whiistles_helper.rb
+++ app/helpers/whiistles_helper.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 
 module WhiistlesHelper
+  include Rails.application.routes.url_helpers
+  
   def show_profile_picture(profile_picture)
     profile_picture.attachment || 'default_profile_picture.png'
   end
-
-  include Rails.application.routes.url_helpers
 
   def cover_image_url(user)
     rails_blob_path(user.cover_image, disposition: 'attachment', only_path: true) if user.cover_image.attached?
   end
 
-  def check_click(whiistle)
-    'click' if whiistle.body.match(/c.*l.*i.*c.*k/i)
-  end
 end
